09fa3db9270032c26891fb11687898381bd14dbe
Vincent Picaud <vincent.picaud@laposte.net>
2016-07-16 08:24:37 +0200

    fix :main no in c tangled code

--- Blog/MinMax/jaccard.c
+++ Blog/MinMax/jaccard.c
@@ -1,31 +1,26 @@
-int main() {
 #include <math.h>
 #include <stdint.h>
 
-double jaccard_C_NaN_aware(uint64_t size,
-			   double *a, double *b)
+double jaccard_C_NaN_aware(uint64_t size, double *a, double *b)
 {
-  double num = 0, den = 0;
+    double num = 0, den = 0;
 
-  for(uint64_t i = 0; i < size; ++i)
+    for(uint64_t i = 0; i < size; ++i)
     {
-      num += fmin(a[i],b[i]);
-      den += fmax(a[i],b[i]);
+        num += fmin(a[i], b[i]);
+        den += fmax(a[i], b[i]);
     }
-  return 1. - num / den;
+    return 1. - num / den;
 }
 
-double jaccard_C_comparison(uint64_t size,
-			    double *a, double *b)
+double jaccard_C_comparison(uint64_t size, double *a, double *b)
 {
-  double num = 0, den = 0;
+    double num = 0, den = 0;
 
-  for(uint64_t i = 0; i < size; ++i)
+    for(uint64_t i = 0; i < size; ++i)
     {
-      num += (a[i] < b[i] ? a[i] : b[i]);
-      den += (a[i] > b[i] ? a[i] : b[i]);
+        num += (a[i] < b[i] ? a[i] : b[i]);
+        den += (a[i] > b[i] ? a[i] : b[i]);
     }
-  return 1. - num / den;
-}
-return 0;
+    return 1. - num / den;
 }
