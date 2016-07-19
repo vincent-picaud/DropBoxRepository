c65201c70dc0ea01a5d1b99bba6cfa782c9ef46a
picaud <vincent.picaud@cea.fr>
2016-07-19 11:49:51 +0200

    work on conv

--- Archive/Draft/DirectConv/directConv.jl
+++ Archive/Draft/DirectConv/directConv.jl
@@ -1,44 +1,34 @@
 
-function compute_direct_conv!{T}(α::StridedVector{T},λ::Int,
-                                 β::StridedVector{T},
-                                 γ::StridedVector{T})
-
-    for k in [1:length(γ)]
-        for i in [1:length(α)]
-            γ[k]+=α[i]*β[k+i*λ]
-        end
-    end
-    
-end
-
 
 # ________________________________________________________________
 
-function scale(λ::Int,range::UnitRange)
+function scale(λ::Int,Ω::UnitRange)
     ifelse(λ>0,
-           UnitRange(λ*start(range),λ*last(range)),
-           UnitRange(λ*last(range),λ*start(range)))
+           UnitRange(λ*start(Ω),λ*last(Ω)),
+           UnitRange(λ*last(Ω),λ*start(Ω)))
 end
 
-function Ωγ1_range(α_range::UnitRange,
+function compute_Ωγ1(Ωα::UnitRange,
                    λ::Int,
-                   β_range::UnitRange)
+                   Ωβ::UnitRange)
     
-    λα_range = scale(λ,α_range)
+    λΩα = scale(λ,Ωα)
 
-    UnitRange(start(β_range)-start(λα_range),last(β_range)-last(λα_range))
+    UnitRange(start(Ωβ)-start(λΩα),
+              last(Ωβ)-last(λΩα))
 end
 
-function Ωγ2_range(α_range::UnitRange,
+function compute_Ωγ2(Ωα::UnitRange,
                    λ::Int,
-                   β_range::UnitRange)
+                   Ωβ::UnitRange)
     
-    λα_range = scale(λ,α_range)
+    λΩα = scale(λ,Ωα)
 
-    UnitRange(start(β_range)-last(λα_range),last(β_range)-start(λα_range))
+    UnitRange(start(Ωβ)-last(λΩα),
+              last(Ωβ)-start(λΩα))
 end
 
-# Left & Right relative complement A\B
+# Left & Right relative complements A\B
 function relelativeComplement_left(A::UnitRange,B::UnitRange)
     UnitRange(start(A),min(last(A),start(B)-1))
 end
@@ -73,88 +63,64 @@ boundaryExtension = Dict(:ZeroPadding=>boundaryExtension_zeroPadding,
 
 
 function direct_conv!{T}(α::StridedVector{T},
-                         α_range::UnitRange,
+                         Ωα::UnitRange,
                          λ::Int,
                          β::StridedVector{T},
                          γ::StridedVector{T},
+                         Ωγ::UnitRange,
                          LeftBoundary::Symbol,
                          RightBoundary::Symbol)
     
-    # sanity check
-    # @assert in(UnitRange(1,length(γ)),γ_range) "bad γ range $γ_range"
-
-#    const first_idx = Int(1) # in C is 0
-    
-    # Avoid the complications of λ<0
-    #
-    negative_λ=false
-
-    if λ<0
-        negative_λ=true
-        λ=-λ
-        reverse!(α)
-        α_range=UnitRange(-last(α_range),-start(α_range))
-    end
-
     # Initialization
-    β_range = UnitRange(1,length(β))
-    γ_range = UnitRange(1,length(γ))
-    tilde_α_range = 1:length(α_range)
+    Ωβ = UnitRange(1,length(β))
+    tilde_Ωα = 1:length(Ωα)
     
     fill!(γ,T(0))
 
-    rΩγ1=intersect(γ_range,Ωγ1_range(α_range,λ,β_range))
-    rΩγ2=intersect(γ_range,Ωγ2_range(α_range,λ,β_range))
-
-#    print("$α_range $rΩγ1 $rΩγ2")
+    rΩγ1=intersect(Ωγ,compute_Ωγ1(Ωα,λ,Ωβ))
+    rΩγ2=intersect(Ωγ,compute_Ωγ2(Ωα,λ,Ωβ))
 
     # rΩγ1 part: no boundary effect
     #
-    
-    
-    offset = λ*(start(α_range)-1)
+    offset = λ*(start(Ωα)-1)
     for k in rΩγ1
-        for i in tilde_α_range
+        for i in tilde_Ωα
             γ[k]+=α[i]*β[k+λ*i+offset]
         end
     end
 
-    # Left part (bounday effect)
+    # Left part
     #
     left_rΩγ2 = relelativeComplement_left(rΩγ2,rΩγ1)
-    left_β = boundaryExtension[LeftBoundary]
+    Φ_left = boundaryExtension[LeftBoundary]
     
     for k in left_rΩγ2
-#        print("Left $k")
-        for i in tilde_α_range
-            γ[k]+=α[i]*left_β(β,k+λ*i+offset)
+        for i in tilde_Ωα
+            γ[k]+=α[i]*Φ_left(β,k+λ*i+offset)
         end
     end
 
-    # Right part (bounday effect)
+    # Right part
     #
     right_rΩγ2 = relelativeComplement_right(rΩγ2,rΩγ1)
-    right_β = boundaryExtension[RightBoundary]
+    Φ_right = boundaryExtension[RightBoundary]
     
     for k in right_rΩγ2
- #       print("Right $k")
-        for i in tilde_α_range
-            γ[k]+=α[i]*right_β(β,k+λ*i+offset)
+        for i in tilde_Ωα
+            γ[k]+=α[i]*Φ_right(β,k+λ*i+offset)
         end
-    end
-
-    # Restore initial α
-
-    #
-    if  negative_λ
-        reverse!(α)
     end
 end
 
 
-function direct_conv!{T}(α::StridedVector{T},α_offset::Int,λ::Int,
+function direct_conv!{T}(α::StridedVector{T},
+                         α_offset::Int,λ::Int,
+
                          β::StridedVector{T},
+
                          γ::StridedVector{T},
+                         Ωγ::UnitRange,
+                         
                          LeftBoundary::Symbol,
                          RightBoundary::Symbol)
 
@@ -167,8 +133,11 @@ function direct_conv!{T}(α::StridedVector{T},α_offset::Int,λ::Int,
                  RightBoundary)
 end
 
-function direct_conv{T}(α::StridedVector{T},α_offset::Int,λ::Int,
+function direct_conv{T}(α::StridedVector{T},
+                        α_offset::Int,λ::Int,
+
                         β::StridedVector{T},
+
                         LeftBoundary::Symbol,
                         RightBoundary::Symbol)
 
@@ -185,7 +154,7 @@ function direct_conv{T}(α::StridedVector{T},α_offset::Int,λ::Int,
     γ
 end
 
-function direct_conv{T}(α::StridedVector{T},α_range::UnitRange,λ::Int,
+function direct_conv{T}(α::StridedVector{T},Ωα::UnitRange,λ::Int,
                         β::StridedVector{T},
                         LeftBoundary::Symbol,
                         RightBoundary::Symbol)
@@ -193,7 +162,7 @@ function direct_conv{T}(α::StridedVector{T},α_range::UnitRange,λ::Int,
     γ = Array{T,1}(length(β))
     
     direct_conv!(α,
-                 α_range,
+                 Ωα,
                  λ,
                  β,
                  γ,
@@ -204,7 +173,7 @@ function direct_conv{T}(α::StridedVector{T},α_range::UnitRange,λ::Int,
 end
 
 function fft_conv{T}(α::StridedVector{T},β::StridedVector{T})
-    @assert isodd(length(α)) ""
+    @assert isodd(length(α)) 
     halfWindow = round(Int,(length(α)-1)/2)
     padded_β = [β[1]*ones(halfWindow); β; β[end]*ones(halfWindow)]
     γ = conv(α[end:-1:1], padded_β)
@@ -213,9 +182,14 @@ end
 
 
 # example
-α=[0,1,0]
-β=[1:10;]
-γ=[1:20;]
+nα=11
+nβ=200
+nγ=3000
 
-direct_conv!(α,1,1,β,γ,:Constant,:Constant)
+α=rand(nα)
+β=rand(nβ)
+γ=rand(nγ)
+
+@assert isodd(length(α)) 
+direct_conv!(α,round(Int,(length(α)-1)/2),1,β,γ,:Constant,:Constant)
 γ
