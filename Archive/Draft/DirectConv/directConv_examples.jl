0938c33a0d8a65b06df0a95936279010edf4fbf4
Vincent Picaud <vincent.picaud@laposte.net>
2016-07-20 08:25:01 +0200

    testing conv

--- Archive/Draft/DirectConv/directConv_examples.jl
+++ Archive/Draft/DirectConv/directConv_examples.jl
@@ -1,9 +1,16 @@
+workspace()
+include("/home/picaud/GitHub/DropBoxRepository/Archive/Draft/DirectConv/DirectConv.jl")
+
 using DirectConv
 
 α=Float64[0,0,1]
-β=zeros(5)
+β=Float64[1:5;]
 
 γ=zeros(10)
 Ωγ=UnitRange(1,10)
 
-direct_conv(α,0,1,β,:Mirror,:Periodic)
+#direct_conv!(α,2,1,β,γ,Ωγ,:Mirror,:Periodic)
+direct_conv!(α,2,1,β,γ,Ωγ,:Periodic,:Mirror)
+
+#direct_conv(α,2,1,β,:ZeroPadding,:ZeroPadding)
+
